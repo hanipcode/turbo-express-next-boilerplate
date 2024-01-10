d7d38d61cc81ca99adf2c91de468b8c8b3c8d8b7
Muhammad Hanif <hanifeij@yahoo.co.id>
2024-01-10 23:12:14 +0700

    feat: initial setup

---
+++ packages/eslint-config/api.js
@@ -0,0 +1,56 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    "eslint-config-turbo",
+    "plugin:import/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["only-warn"],
+  globals: {
+    Node: true,
+  },
+  env: {
+    node: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: [
+    // Ignore dotfiles
+    ".*.js",
+    "node_modules/",
+    "dist/",
+  ],
+  overrides: [
+    {
+      files: ["*.js?(x)", "*.ts?(x)"],
+    },
+  ],
+  rules: {
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type",
+        ],
+      },
+    ],
+  },
+};
